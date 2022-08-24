f77f76a1f563053ea579b5d393c25bcf30a6440d
Daniel Contreras <dantnadoriginal@gmail.com>
2022-08-23 19:11:18 -0500

    Create date module, refactor code to use constants

--- app.js
+++ app.js
@@ -1,25 +1,18 @@
 const express = require('express');
 const app = express();
-let ejs = require("ejs");
+const ejs = require("ejs");
+const date = require(__dirname + '/date.js')
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 app.set("view engine", "ejs");
 
-let tasks = [];
-let workTasks = [];
+const tasks = [];
+const workTasks = [];
 
 app.get("/", function(req, res){
-    let today = new Date();
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    }
-    let day = today.toLocaleDateString("en-US", options);
-
     res.render("list", {
-      listTitle: day,
+      listTitle: date.getDate(),
       listOfTasks: tasks
     }); 
 });
